eb04139f79a79bbbeeac16895fab5607b0a4d863
honeybeeweng <kelly14039@hotmail.com>
2024-06-24 20:01:51 +0800

    24/06/24上傳
    24/06/24上傳

--- js/master.js
+++ js/master.js
@@ -11,6 +11,8 @@
 // }
 // 等同下列:
 const isMobile = $(window).width() <= 480;
+const $grid = $('#Grid');
+const $nav = $('#Nav');
 
 function setGrid() {
     // // if(isMobile == true) { 等同下列
@@ -21,25 +23,74 @@ function setGrid() {
     // }
     // 也等同下面條列:
     if(isMobile) {
-        $('#Grid').addClass('grid-2');
+        // $('#Grid').addClass('grid-2');
+        $grid.addClass('grid-2');
         return;
     }
-        $('#Grid').addClass('grid-3');
+        // $('#Grid').addClass('grid-3');
+        $grid.addClass('grid-3');
 
 }
 
-setGrid();
+function setScroll(){
+    $(window).scroll(function(){
+        
+        // isMobile:true
+        // !isMobile:false
+        // if (isMobile == true) {
+            
+        // console.log($(window).scrollTop());
+            
+        if(!isMobile) return;
 
-$(window).scroll(function(){
-    // console.log($(window).scrollTop());
-    if ($(window).scrollTop() == 0) {
-        $('#Nav').removeClass('nav-active');
-        console.log(0)
-    }else {
-        $('#Nav').addClass('nav-active');
-        console.log('要套用 class')
-    }
-});
+        // if ($(window).scrollTop() == 0) {
+        if ($(this).scrollTop() == 0) {
+
+           $nav.removeClass('nav-active');
+                
+           // console.log(0)
+
+        }else {
+            $nav.addClass('nav-active');
+                
+            // console.log('要套用 class')
+        }
+    });
+}
+
+// 06/19寫法
+// function setScroll(){
+//     $(window).scroll(function(){
+//         if (isMobile == true) {
+//             // console.log($(window).scrollTop());
+//             if ($(window).scrollTop() == 0) {
+//                 $nav.removeClass('nav-active');
+                
+//                 // console.log(0)
+                
+//             }else {
+//                 $nav.addClass('nav-active');
+                
+//                 // console.log('要套用 class')
+//             }
+//         }
+//     });
+// }
+
+
+// 其他動畫載入的函式庫
+function setInit() {
+    setGrid();
+}
+
+// 其他事件的函式庫
+function setEvent() {
+    setScroll();
+}
+
+setInit();
+setEvent();
+
 
 
 
